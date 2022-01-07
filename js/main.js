33672366cca4b95d1a77dc285170ee32f7bc4fe5
Randy López <77851376+ralfxdev@users.noreply.github.com>
2022-01-07 12:01:22 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// function contact
+const $form = document.querySelector('#form');
+const $buttonMailto = document.querySelector('#mail');
+
+$form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event){
+   event.preventDefault();
+   const form = new FormData(this);
+   $buttonMailto.setAttribute('href', `mailto:totaleserviceu22@gmail.com?subject=Nombre: ${form.get('nombre')} | Correo: ${form.get('correo')} | Asunto: ${form.get('asunto')} &body=${form.get('mensaje')}`)
+   $buttonMailto.click()
+};
+
