15721269be60a03cba32860e3e1d91838f28acc0
Randy López <77851376+ralfxdev@users.noreply.github.com>
2022-01-07 13:12:01 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $form.addEventListener('submit', handleSubmit);
 function handleSubmit(event){
    event.preventDefault();
    const form = new FormData(this);
-   $buttonMailto.setAttribute('href', `mailto:totaleserviceu22@gmail.com?subject=Nombre: ${form.get('nombre')} | Correo: ${form.get('correo')} | Asunto: ${form.get('asunto')} &body=${form.get('mensaje')}`)
+   $buttonMailto.setAttribute('href', `mailto:totaleservicesu22@gmail.com?subject=Nombre: ${form.get('nombre')} | Correo: ${form.get('correo')} | Asunto: ${form.get('asunto')} &body=${form.get('mensaje')}`)
    $buttonMailto.click()
 };
 
